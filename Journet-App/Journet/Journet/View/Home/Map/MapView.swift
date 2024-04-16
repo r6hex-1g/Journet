23afe6213fba37ed97d853db9c9802b157aa621b
라 희 <sechan963@gmail.com>
2024-04-16 16:29:11 +0900

    [ADD] : 🗺️ MapView Add

--- Journet-App/Journet/Journet/View/Home/Map/MapView.swift
+++ Journet-App/Journet/Journet/View/Home/Map/MapView.swift
@@ -1,10 +1,32 @@
 import SwiftUI
+import MapKit
+import CoreLocation
 
+// MARK: - Map View
 struct MapView: View {
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion()
+    @State var isShowMap: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isShowMap {
+            Map(coordinateRegion: $region, showsUserLocation: true)
+        }
+        
+        Button("현재 위치의 지도 보기") {
+            let manager = CLLocationManager()
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.requestWhenInUseAuthorization()
+            
+            let latitude = manager.location?.coordinate.latitude
+            let longitude = manager.location?.coordinate.longitude
+            
+            region = MKCoordinateRegion(
+                
+            )
+        }
     }
 }
+
 
 #Preview {
     MapView()
