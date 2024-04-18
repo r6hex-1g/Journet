fcc9fa323d76fe6d992c179bf9e36bdf77ac5586
라 희 <sechan963@gmail.com>
2024-04-19 02:16:13 +0900

    [STYLE] : 🎨 IntroductionView apply

--- Journet-App/Journet/Journet/View/Welcome/Introduction/IntroductionPage/IntroductionOne.swift
+++ Journet-App/Journet/Journet/View/Welcome/Introduction/IntroductionPage/IntroductionOne.swift
@@ -11,20 +11,21 @@ struct IntroductionOne: View {
     }
 }
 
+//MARK: - Header Views
 struct I1HeaderView: View {
     let biggerText = Font.system(size: 35.0)
     
     var body: some View {
         VStack(alignment: .leading) {
-            Image(systemName: "person.line.dotted.person.fill")
+            Image(systemName: "square.and.pencil")
                 .resizable()
                 .foregroundColor(Color("MainColor"))
-                .frame(width: 80, height: 40)
+                .frame(width: 60, height: 60)
                 .scaledToFit()
             
             VStack(alignment: .leading, spacing: 10.0) {
                 VStack(alignment: .leading, spacing: -5.0) {
-                    Text("Connect to")
+                    Text("Write to")
                         .font(biggerText)
                         .fontWeight(.bold)
                         .fontDesign(.rounded)
@@ -35,10 +36,22 @@ struct I1HeaderView: View {
                         .fontDesign(.rounded)
                 }
                 
-                Text("일기를 통해 타인과 연결되어보세요.")
-                Text("다른 사람들과 다른 모습으로 일기를 나누고, 자신만의 공간을 나누며 이런 곳이 있음을 나눌 수 있습니다.")
-                    .font(.caption)
+                Text("나만의 일기를 써보새요.")
+                    .fontWeight(.bold)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("평소와는 다른 공간에서의 일기를 저장하고,")
+                        .font(.footnote)
+                    Text("그 일기에서 그 날에 대한 감정들을 저잘하고,")
+                        .font(.footnote)
+                    Text("언제든 필요하다면 꺼내보고 추억을 회상할 수 있도록 도와주기도 하는 공간입니다.")
+                        .font(.footnote)
+                }
             }
         }
     }
+}
+
+#Preview {
+    IntroductionOne()
 }
