8e1e5f6b85430fd1c796767cfa97ee8c916dd81b
라 희 <sechan963@gmail.com>
2024-04-18 15:57:31 +0900

    [STYLE] : 🎨 Header design apply

--- Journet-App/Journet/Journet/View/Diaries/DHeaderView.swift
+++ Journet-App/Journet/Journet/View/Diaries/DHeaderView.swift
@@ -1,11 +1,21 @@
 import SwiftUI
 
 struct DHeaderView: View {
+    
     var body: some View {
-        HStack {
+        Spacer(minLength: 20)
+        HStack(alignment: .center, spacing: 130) {
             Text("Diaries")
+                .fontWeight(.bold)
+                .fontDesign(.rounded)
+            Image(systemName: "plus.circle.fill")
+                .resizable()
+                .frame(width: 30, height: 30, alignment: .center)
+                .foregroundColor(Color("MainColor"))
+                .onTapGesture {
+                    //
+                }
         }
-        .padding()
     }
 }
 
