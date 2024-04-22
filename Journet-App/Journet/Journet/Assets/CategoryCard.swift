8f0e2607b2ad62a0dee2e943592bf888adaf7051
라 희 <sechan963@gmail.com>
2024-04-22 11:31:24 +0900

    [STYLE] : 🎨 Cards edit and apply

--- Journet-App/Journet/Journet/Assets/CategoryCard.swift
+++ Journet-App/Journet/Journet/Assets/CategoryCard.swift
@@ -2,17 +2,17 @@ import SwiftUI
 
 struct CategoryCard: View {
     
-    let normalText = Font.system(size: 17.5)
+    let normalText = Font.system(size: 15)
+    let descriptionText = Font.system(size: 10)
     
     var imoji: String
     var title: String
     var description: String
     
     var body: some View {
-        
         VStack(alignment: .center, spacing: 10) {
             Text(imoji)
-                .font(.title)
+                .font(.title3)
             
             VStack(alignment: .center, spacing: 5) {
                 Text(title)
@@ -21,15 +21,15 @@ struct CategoryCard: View {
                     .fontDesign(.rounded)
                     .foregroundColor(Color("MainColor"))
                 Text(description)
-                    .font(.caption2)
+                    .font(descriptionText)
                     .foregroundColor(Color(.systemGray))
             }
             .layoutPriority(100)
         }
-        .padding(.top, 20)
-        .padding(.bottom, 20)
-        .padding(.leading, 50)
-        .padding(.trailing, 50)
+        .padding(.top, 15)
+        .padding(.bottom, 15)
+        .padding(.leading, 30)
+        .padding(.trailing, 30)
         .overlay(
             RoundedRectangle(cornerRadius: 100)
                 .stroke(Color(.sRGB, red: 97/255, green: 144/255, blue: 244/255, opacity: 0.3), lineWidth: 1)
