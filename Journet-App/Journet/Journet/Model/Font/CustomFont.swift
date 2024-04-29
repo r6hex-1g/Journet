1008c20ac2fa2bef510397f6e036b0e48fac7097
라 희 <sechan963@gmail.com>
2024-04-29 12:03:26 +0900

    [STYLE] : 🎨font add and apply

---
+++ Journet-App/Journet/Journet/Model/Font/CustomFont.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+extension Font {
+    
+    // Heading Font
+    static let Hanbit37: Font = .custom("KCC-Hanbit", size: 37)
+    static let Hanbit30: Font = .custom("KCC-Hanbit", size: 30)
+    
+    // Sub-Heading Font
+    static let Hanbit29: Font = .custom("KCC-Hanbit", size: 29)
+    static let Hanbit25: Font = .custom("KCC-Hanbit", size: 25)
+    
+    // medium Text Font
+    static let Hanbit23: Font = .custom("KCC-Hanbit", size: 23)
+    static let Hanbit20: Font = .custom("KCC-Hanbit", size: 20)
+    
+    // caption Font
+    static let Hanbit15: Font = .custom("KCC-Hanbit", size: 15)
+    static let Hanbit10: Font = .custom("KCC-Hanbit", size: 10)
+}
