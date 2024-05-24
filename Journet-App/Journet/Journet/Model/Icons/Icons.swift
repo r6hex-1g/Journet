c38f1cd5717b697bc7680a7cad0249c90208b330
라 희 <105037764+r6hex-1g@users.noreply.github.com>
2024-05-24 11:19:38 +0900

    [DELETE] : 🔥 deleted to App files

--- Journet-App/Journet/Journet/Model/Icons/Icons.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-//MARK: - Notification View Icon
-enum Icons: String, CaseIterable {
-    case notificationIcon = "Notification_bell"
-}
--- Journet-App/Journet/Journet/Model/Tab/Tab.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-//MARK: - Tab's
-enum Tab: String, CaseIterable {
-    case home = "Home"
-    case map = "world_globe"
-    case profiles = "Account"
-}
